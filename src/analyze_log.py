c84e3952723d758fb8efc1bb54eae0cdd0ae0d65
Istvan <olivaistvan@gmail.com>
2023-01-07 16:51:33 -0300

    feat: (analyze_log) Implements read_file()

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,13 @@
-def analyze_log(path_to_file):  # Initial commit
+import csv
+
+def read_file(path_to_file):
+    if not path_to_file.endswith('.csv'):
+        raise FileNotFoundError(f"Extensão inválida: '{path_to_file}'")
+    try:
+        with open(path_to_file, mode='r', encoding='utf-8') as file:
+            return list(csv.reader(file))
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Arquivo inexistente: '{path_to_file}'")
+
+def analyze_log(path_to_file):
     raise NotImplementedError
