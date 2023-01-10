b693a3dbcf1ac122e8b61d1ba05a83ef6a06f969
Istvan <olivaistvan@gmail.com>
2023-01-10 20:20:19 -0300

    feat: (analyze_log) def write_file

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,5 +1,6 @@
 import csv
 
+
 def read_file(path_to_file):
     if not path_to_file.endswith('.csv'):
         raise FileNotFoundError(f"Extensão inválida: '{path_to_file}'")
@@ -8,6 +9,15 @@ def read_file(path_to_file):
             return list(csv.reader(file))
     except FileNotFoundError:
         raise FileNotFoundError(f"Arquivo inexistente: '{path_to_file}'")
+
+
+def write_file(content, path_to_file):
+    try:
+        with open("data/mkt_campaign.txt", mode="w") as analyze_log:
+            analyze_log.write(content)
+    except ValueError:
+        raise ValueError
+
 
 def analyze_log(path_to_file):
     raise NotImplementedError
